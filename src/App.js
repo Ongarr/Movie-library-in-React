9baad334308f1d501cd353df034d278be1caad3e
Ongarr <jakub.kulon@hotmail.com>
2020-02-22 20:40:43 +0100

    Get done a API request and promise resolve. I left a simply console.log for testing.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import api from './movie';
+
+const apiRequest = new api();
+
+const movie = 'bean';
+
+apiRequest.getMovie(movie)
+  .then(data => updateUI(data))
+  .catch(err => console.log(err))
+
+  const updateUI = (data) => {
+    const { results, total_results, total_pages } = data;
+
+    console.log(results)
+    console.log(total_results)
+    console.log(total_pages)
+  }
+
 
 function App() {
   return (
---
+++ src/movie.js
@@ -0,0 +1,12 @@
+export default class Movie {
+    
+    key = 'ae56d5e33eecc34a48f563c98dd330ad';
+    baseUrl = 'https://api.themoviedb.org/3/search/movie?api_key=';
+
+    getMovie = async (movie) => {
+        const movieQuery = `${this.baseUrl}${this.key}&language=en-EN&query=${movie}&page=1&include_adult=false`;
+        const response = await fetch(movieQuery);
+        const data = await response.json(); 
+        return data;
+    }
+}
