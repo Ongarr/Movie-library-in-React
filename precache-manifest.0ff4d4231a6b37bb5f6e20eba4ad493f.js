64b5422dc3a646014c23824c299073e3265a4750
root <root@pop-os.localdomain>
2020-08-18 20:49:53 +0200

    Updates

---
+++ precache-manifest.0ff4d4231a6b37bb5f6e20eba4ad493f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d80a4643940fde1b20f48ad2351fb13",
+    "url": "/Movie-library-in-React/index.html"
+  },
+  {
+    "revision": "6a7456cc190feba66907",
+    "url": "/Movie-library-in-React/static/css/main.4e54e323.chunk.css"
+  },
+  {
+    "revision": "8470a61ba844c133c0f2",
+    "url": "/Movie-library-in-React/static/js/2.082cbb3d.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/Movie-library-in-React/static/js/2.082cbb3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a7456cc190feba66907",
+    "url": "/Movie-library-in-React/static/js/main.5680043e.chunk.js"
+  },
+  {
+    "revision": "9feb7e5c6df5cb30f79a",
+    "url": "/Movie-library-in-React/static/js/runtime-main.d4152801.js"
+  }
+]);
